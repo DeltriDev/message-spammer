e618c9d936307aa0da6484b16e6dc12363347384
SamAlexandros <98485800+SamAlexandros@users.noreply.github.com>
2022-08-17 10:58:57 +0300

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,10 +1,20 @@
-from pynput.keyboard import Key, Controller
 import time
+import os
+
+try:
+    import pynput
+except ImportError:
+    input(f"Module pynput not installed, to install run '{'py -3' if os.name == 'nt' else 'python3.8'} -m pip install pynput'\nPress enter to exit")
+    exit()
+from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
 do = True
-word = input("What would you like to spam?")
-print("You have 3 seconds to swap to messanger app")
+print("Made by SamAlexandros#2211 (TacoSuaceDev)")
+time.sleep(1)
+print("")
+word = input("What would you like to spam? ")
+print("Spam starts in:")
 print("3...")
 time.sleep(1)
 print("2...")
